537691a05d26e9b6c5329ca5cec8243c08c2b383
Lucien Shui <lucien@lucien.ink>
2024-12-18 20:57:09 +0800

    Add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
 import {Button} from "@/components/ui/button";
+import {ThemeProvider} from "@/components/theme-provider";
+import {ModeToggle} from "@/components/mode-toggle.tsx";
+import Layout from "@/app/layout"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-      </div>
-    </>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <Layout>
+        <ModeToggle></ModeToggle>
+        <div>
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+        </div>
+      </Layout>
+    </ThemeProvider>
   )
 }
 
