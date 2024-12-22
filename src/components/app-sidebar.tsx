9249faa1d8d2c83f88e75ba080f8ce06659055e3
Lucien Shui <lucien@lucien.ink>
2024-12-22 23:01:24 +0800

    Add lazy load support

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -14,56 +14,12 @@ import {
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarMenu,
-  SidebarMenuBadge,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarMenuSub,
   SidebarRail,
 } from "@/components/ui/sidebar"
 
-// This is sample data.
-const data = {
-  changes: [
-    {
-      file: "README.md",
-      state: "M",
-    },
-    {
-      file: "api/hello/route.ts",
-      state: "U",
-    },
-    {
-      file: "app/layout.tsx",
-      state: "M",
-    },
-  ],
-  tree: [
-    [
-      "app",
-      [
-        "api",
-        ["hello", ["route.ts"]],
-        "page.tsx",
-        "layout.tsx",
-        ["blog", ["page.tsx"]],
-      ],
-    ],
-    [
-      "components",
-      ["ui", "button.tsx", "card.tsx"],
-      "header.tsx",
-      "footer.tsx",
-    ],
-    ["lib", ["util.ts"]],
-    ["public", "favicon.ico", "vercel.svg"],
-    ".eslintrc.json",
-    ".gitignore",
-    "next.config.js",
-    "tailwind.config.js",
-    "package.json",
-    "README.md",
-  ],
-}
 
 type Resource = {
   id: string;
@@ -112,6 +68,61 @@ export function AppSidebar({...props}: React.ComponentProps<typeof Sidebar>) {
     loadInitialData()
   }, []);
 
+  const handleExpand = async (namespace: string, spaceType: string, parentId: string) => {
+    if (!(parentId in child)) {
+      const childData = await fetchResources(namespace, spaceType, parentId);
+      setChild((prev) => ({
+        ...prev,
+        [parentId]: childData,
+      }));
+    }
+    setIsExpanded((prev) => ({
+      ...prev,
+      [parentId]: !isExpanded[parentId]
+    }));
+  };
+
+  function Tree({namespace, spaceType, resource}: { namespace: string, spaceType: string, resource: Resource }) {
+    if (resource.childCount > 0) {
+      return (
+        <SidebarMenuItem>
+          <Collapsible
+            className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
+            open={isExpanded[resource.id]}
+          >
+            <CollapsibleTrigger asChild>
+              <SidebarMenuButton>
+                <ChevronRight
+                  className="transition-transform"
+                  onClick={() => handleExpand(namespace, spaceType, resource.id)}
+                />
+                <Folder/>
+                {resource.name}
+              </SidebarMenuButton>
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              <SidebarMenuSub>
+                {(child[resource.id] ?? []).length > 0 &&
+                  child[resource.id].map((resource: Resource) => (
+                    <Tree key={resource.id} resource={resource} namespace={namespace} spaceType={spaceType}/>
+                  ))
+                }
+              </SidebarMenuSub>
+            </CollapsibleContent>
+          </Collapsible>
+        </SidebarMenuItem>
+      )
+    }
+    return (
+      <SidebarMenuButton
+        className="data-[active=true]:bg-transparent"
+      >
+        <File/>
+        {resource.name}
+      </SidebarMenuButton>
+    )
+  }
+
   return (
     <Sidebar {...props}>
       <SidebarContent>
@@ -120,7 +131,7 @@ export function AppSidebar({...props}: React.ComponentProps<typeof Sidebar>) {
           <SidebarGroupContent>
             <SidebarMenu>
               {(child[rootResourceId["private"]] ?? []).map((resource) => (
-                <Tree key={resource.id} resource={resource} child={child}/>
+                <Tree key={resource.id} resource={resource} namespace={"test"} spaceType={"private"}/>
               ))
               }
             </SidebarMenu>
@@ -132,39 +143,4 @@ export function AppSidebar({...props}: React.ComponentProps<typeof Sidebar>) {
   )
 }
 
-function Tree({resource, child}: { resource: Resource, child: Record<string, Resource[]> }) {
-  if (resource.childCount > 0) {
-    return (
-      <SidebarMenuItem>
-        <Collapsible
-          className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
-        >
-          <CollapsibleTrigger asChild>
-            <SidebarMenuButton>
-              <ChevronRight className="transition-transform"/>
-              <Folder/>
-              {resource.name}
-            </SidebarMenuButton>
-          </CollapsibleTrigger>
-          <CollapsibleContent>
-            <SidebarMenuSub>
-              {(child[resource.id] ?? []).length > 0 &&
-                child[resource.id].map((resource: Resource) => (
-                  <Tree key={resource.id} resource={resource} child={child}/>
-                ))
-              }
-            </SidebarMenuSub>
-          </CollapsibleContent>
-        </Collapsible>
-      </SidebarMenuItem>
-    )
-  }
-  return (
-    <SidebarMenuButton
-      className="data-[active=true]:bg-transparent"
-    >
-      <File/>
-      {resource.name}
-    </SidebarMenuButton>
-  )
-}
+
