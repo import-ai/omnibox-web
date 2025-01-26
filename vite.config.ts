9fb677b27e634d30fe672b432a663506761cac68
seiry yu <4397354+seiry@users.noreply.github.com>
2025-01-26 20:49:34 +1100

    Add Jotai Babel plugins for debug labeling and React refresh in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from "node:path";
-import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import jotaiDebugLabel from "jotai/babel/plugin-debug-label";
+import jotaiReactRefresh from "jotai/babel/plugin-react-refresh";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -9,6 +11,7 @@ export default defineConfig(({ command, mode }) => {
     plugins: [
       react({
         babel: {
+          plugins: [jotaiDebugLabel, jotaiReactRefresh],
           presets: ["jotai/babel/preset"],
         },
       }),
