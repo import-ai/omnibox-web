d9419ea0a807d3216c4b707785174724829d3f2b
Lucien Shui <lucien@lucien.ink>
2025-04-30 11:24:38 +0800

    fix(resource): Fix resource modify info's whitespace

--- src/page/resource/utils.ts
+++ src/page/resource/utils.ts
@@ -9,12 +9,14 @@ export function getTime(resource: Resource | null) {
   if (resource.updatedAt) {
     return (
       i18next.t('updated') +
+      ' ' +
       formatDistanceToNow(new Date(resource.updatedAt), { addSuffix: true })
     );
   }
   if (resource.createdAt) {
     return (
       i18next.t('created') +
+      ' ' +
       formatDistanceToNow(new Date(resource.createdAt), { addSuffix: true })
     );
   }
