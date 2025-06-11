795bdd5331fe831de8b513b8cfc7c1f9db83ad1b
Lucien <lucien@lucien.ink>
2025-06-11 20:22:41 +0800

    chore(folder): Revert folder content display

--- src/page/resource/page.tsx
+++ src/page/resource/page.tsx
@@ -6,7 +6,6 @@ import { useTranslation } from 'react-i18next';
 import 'vditor/dist/index.css';
 import '@/styles/vditor-patch.css';
 import App from '@/hooks/app.class';
-import { http } from '@/lib/request.ts';
 
 interface IProps {
   app: App;
@@ -17,8 +16,6 @@ export default function Page(props: IProps) {
   const { app, resource } = props;
   const { t } = useTranslation();
   const [open, onOpen] = useState(true);
-  const title: string = `#  ${resource.name || t('untitled')}`;
-  const [folderContent, setFolderContent] = useState<string>(title);
 
   useEffect(() => {
     return app.on('resource_children', (visible: boolean) => {
@@ -26,33 +23,7 @@ export default function Page(props: IProps) {
     });
   }, []);
 
-  useEffect(() => {
-    if (resource.resource_type === 'folder') {
-      http
-        .get(`/namespaces/${resource.namespace.id}/resources/query`, {
-          params: {
-            namespace: resource.namespace.id,
-            spaceType: resource.space_type,
-            parentId: resource.id,
-          },
-        })
-        .then((response: Resource[]) => {
-          let markdown: string = title;
-          if (response.length > 0) {
-            for (const r of response) {
-              markdown += `\n- [${r.name || t('untitled')}](./${r.id})`;
-            }
-          }
-          setFolderContent(markdown);
-        });
-    }
-    return () => setFolderContent('');
-  }, [resource]);
-
   if (open) {
-    if (resource.resource_type === 'folder') {
-      return <Render content={folderContent} />;
-    }
     return (
       <Render
         content={`# ${resource.name || t('untitled')}\n${resource.content || ''}`}
