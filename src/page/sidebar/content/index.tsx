2713ef880aea0b14899ee9a3397081eb146e72a3
hewenguang <wenguang.fe@gmail.com>
2025-07-08 09:37:51 +0800

    feat(dnd): add touch support for dnd

--- src/page/sidebar/content/index.tsx
+++ src/page/sidebar/content/index.tsx
@@ -3,9 +3,10 @@ import { useState } from 'react';
 import group from '@/lib/group';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 import { ISidebarProps } from '@/page/sidebar/interface';
-import { SidebarContent } from '@/components/ui/sidebar';
 import type { IResourceData, SpaceType } from '@/interface';
+import { SidebarContent, useSidebar } from '@/components/ui/sidebar';
 
 export interface IProps extends Omit<ISidebarProps, 'spaceType'> {
   onDrop: (item: IResourceData, target: IResourceData | null) => void;
@@ -13,6 +14,7 @@ export interface IProps extends Omit<ISidebarProps, 'spaceType'> {
 
 export default function Content(props: IProps) {
   const { data, resourceId, onDrop } = props;
+  const { isMobile } = useSidebar();
   const [target, onTarget] = useState<IResourceData | null>(null);
   const handleDrop = (resource: IResourceData, item: IResourceData | null) => {
     onDrop(resource, item);
@@ -20,7 +22,7 @@ export default function Content(props: IProps) {
   };
 
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
       <SidebarContent>
         {Object.keys(data)
           .sort()
