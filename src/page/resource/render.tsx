3f3a01fd541773765eda5fafd6a32f38d5b10dec
Lucien <lucien@lucien.ink>
2025-07-18 00:23:41 +0800

    fix(img): Convert sname to camel, support <img> tag

--- src/page/resource/render.tsx
+++ src/page/resource/render.tsx
@@ -5,15 +5,28 @@ interface IProps {
   resource: Resource;
 }
 
+function snakeToCamelCase(text: string): string {
+  const result: string = text.replace(/([-_][a-z])/g, (group) =>
+    group.toUpperCase().replace('-', '').replace('_', ''),
+  );
+  console.log({ text, result });
+  return result;
+}
+
 function embedImage(resource: Resource): string {
   let content: string = resource.content || '';
   if (resource.attrs?.images) {
     const images: Record<string, string> = resource.attrs?.images || {};
     for (const [key, value] of Object.entries(images)) {
       content = content.replaceAll(
-        `(${key})`,
-        `(data:image/jpeg;base64,${value})`,
+        `](${snakeToCamelCase(key)})`,
+        `](data:image/jpeg;base64,${value})`,
       );
+      content = content.replaceAll(
+        ` src="${snakeToCamelCase(key)}"`,
+        ` src="data:image/jpeg;base64,${value}"`,
+      );
+      console.log({ content });
     }
   }
   return content;
