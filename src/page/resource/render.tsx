2df3f0b65f0111f6a4b4b7cc14b27ad4222b9565
Lucien <lucien@lucien.ink>
2025-07-17 23:54:29 +0800

    feat(md): Add images render support

--- src/page/resource/render.tsx
+++ src/page/resource/render.tsx
@@ -1,11 +1,26 @@
 import { Markdown } from '@/components/markdown';
+import { Resource } from '@/interface';
 
 interface IProps {
-  content: string;
+  resource: Resource;
+}
+
+function embedImage(resource: Resource): string {
+  let content: string = resource.content || '';
+  if (resource.attrs?.images) {
+    const images: Record<string, string> = resource.attrs?.images || {};
+    for (const [key, value] of Object.entries(images)) {
+      content = content.replaceAll(
+        `(${key})`,
+        `(data:image/jpeg;base64,${value})`,
+      );
+    }
+  }
+  return content;
 }
 
 export default function Render(props: IProps) {
-  const { content } = props;
+  const { resource } = props;
 
-  return <Markdown content={content} />;
+  return <Markdown content={embedImage(resource)} />;
 }
