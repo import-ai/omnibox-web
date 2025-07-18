dc42d910011e87cb09a865798bfc629727d37f05
hewenguang <wenguang.fe@gmail.com>
2025-07-18 17:48:58 +0800

    fix(comment): fix comments

--- src/page/user/login/index.tsx
+++ src/page/user/login/index.tsx
@@ -16,11 +16,6 @@ export default function LoginPage() {
           </p>
         </div>
         <div className="grid gap-6">
-          <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-            <span className="bg-white dark:bg-[#171717] text-muted-foreground relative z-10 px-2">
-              {t('login.or_continue')}
-            </span>
-          </div>
           <LoginForm />
         </div>
       </div>
