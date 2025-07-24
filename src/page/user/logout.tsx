3aa05019f8a8b74b72f76b3e73a9236821f1bf8a
Lucien <lucien@lucien.ink>
2025-07-24 22:06:58 +0800

    chore(cookie): Add reamoveGlobalCredential

--- src/page/user/logout.tsx
+++ src/page/user/logout.tsx
@@ -1,13 +1,13 @@
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
+import { removeGlobalCredential } from '@/page/user/util';
 
 export function Logout() {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const handleLogout = () => {
-    localStorage.removeItem('uid');
-    localStorage.removeItem('token');
+    removeGlobalCredential();
     navigate('/user/login', { replace: true });
   };
 
--- src/page/user/util.tsx
+++ src/page/user/util.tsx
@@ -3,3 +3,10 @@ export function setGlobalCredential(userId: string, token: string) {
   localStorage.setItem('token', token);
   document.cookie = `token=${token}; path=/; secure; samesite=strict`;
 }
+
+export function removeGlobalCredential() {
+  localStorage.removeItem('uid');
+  localStorage.removeItem('token');
+  document.cookie =
+    'token=; path=/; secure; samesite=strict; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+}
