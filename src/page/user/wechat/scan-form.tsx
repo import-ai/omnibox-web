eaca1f2a12b5888a5c8c05bbf577c00966586f74
hewenguang <wenguang.fe@gmail.com>
2025-07-24 16:03:04 +0800

    feat(wechat): support i18n

--- src/page/user/wechat/scan-form.tsx
+++ src/page/user/wechat/scan-form.tsx
@@ -2,8 +2,10 @@ import { cn } from '@/lib/utils';
 import { http } from '@/lib/request';
 import { LoaderCircle } from 'lucide-react';
 import { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export function ScanForm() {
+  const { i18n } = useTranslation();
   const [opacity, setOpacity] = useState(true);
   const [loading, setLoading] = useState(true);
 
@@ -27,6 +29,7 @@ export function ScanForm() {
           appid: response.app_id,
           color_scheme: colorScheme,
           redirect_uri: response.redirect_uri,
+          lang: i18n.language == 'en' ? 'en' : 'cn',
           href: 'data:text/css;base64,LndlYl9xcmNvZGVfcGFuZWxfYXJlYSAud2ViX3FyY29kZV9wYW5lbF9ub3JtYWxfbG9naW57DQogIHBhZGRpbmctdG9wOiAxMHB4Ow0KfQ0KDQoud2ViX3FyY29kZV9wYW5lbF9hcmVhIC53ZWJfcXJjb2RlX3BhbmVsX3F1aWNrX2xvZ2luew0KICBwYWRkaW5nLXRvcDogMTAwcHg7DQp9',
           onReady: function (isReady: boolean) {
             if (!isReady) {
