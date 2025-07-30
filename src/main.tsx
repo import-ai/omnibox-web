f4c4f67a4392faea9e775ea47b4fee254b039440
hewenguang <wenguang.fe@gmail.com>
2025-07-30 20:09:33 +0800

    feat(hash): support log git hash

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,5 @@ import './i18n';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
+
+console.log('当前 OmniBox 版本：', import.meta.env.VITE_APP_VERSION);
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import path from 'node:path';
-import { defineConfig, loadEnv } from 'vite';
+import { execSync } from 'child_process';
 import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+
+const gitHash = execSync('git rev-parse --short HEAD').toString().trim();
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
+    define: {
+      'import.meta.env.VITE_APP_VERSION': JSON.stringify(gitHash),
+    },
     plugins: [react()],
     resolve: {
       alias: {
