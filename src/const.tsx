f2ad1bb88e9fc2b167af3d5f038946df00104625
Lucien <lucien@lucien.ink>
2025-08-11 06:00:05 +0800

    chore(move): Optimize api param name

--- src/const.tsx
+++ src/const.tsx
@@ -1,6 +1,6 @@
 export const VDITOR_CDN: string = '/assets/vditor';
 export const ALLOW_FILE_EXTENSIONS =
-  '.md,.doc,.ppt,.docx,.pptx,.txt,.pdf,.wav,.mp3,.pcm,.opus,.webm';
+  '.md,.doc,.ppt,.docx,.pptx,.txt,.pdf,.wav,.mp3,.m4a,.pcm,.opus,.webm';
 export const SITE_NAME = 'OmniBox';
 export const LAZY_LOAD_IMAGE: string =
   '/assets/vditor/dist/images/img-loading.svg';
