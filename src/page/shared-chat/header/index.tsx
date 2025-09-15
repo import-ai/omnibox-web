33346fccba50dfca4c588ff9807a429526383214
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-09-15 21:07:47 +0800

    feat(share): add shared-chat page

---
+++ src/page/shared-chat/header/index.tsx
@@ -0,0 +1,9 @@
+import TitleSection from './title';
+
+export default function Header() {
+  return (
+    <div className="flex items-center w-full h-12 px-4 border-b">
+      <TitleSection />
+    </div>
+  );
+}
