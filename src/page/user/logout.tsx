1fef7926fc7307d41b7bd5d22a1ab2b7576a45a3
hewenguang <wenguang.fe@gmail.com>
2025-09-17 19:45:37 +0800

    fix(logout): remove cookie when logout

--- src/page/user/logout.tsx
+++ src/page/user/logout.tsx
@@ -1,22 +1,34 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/button';
+import { http } from '@/lib/request';
 import { removeGlobalCredential } from '@/page/user/util';
 
 export function Logout() {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const [loading, onLoading] = useState(false);
   const handleLogout = () => {
-    removeGlobalCredential();
-    document.body.style.pointerEvents = '';
-    navigate('/user/login', { replace: true });
+    onLoading(true);
+    http
+      .post('logout')
+      .then(() => {
+        removeGlobalCredential();
+        document.body.style.pointerEvents = '';
+        navigate('/user/login', { replace: true });
+      })
+      .catch(() => {
+        onLoading(false);
+      });
   };
 
   return (
     <Button
       size="sm"
       variant="ghost"
+      loading={loading}
       onClick={handleLogout}
       className="w-full justify-start text-muted-foreground"
     >
