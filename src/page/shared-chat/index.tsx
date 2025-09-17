d9aaadce792037c75b5934c1beb8736cfd20243a
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-09-17 23:03:37 +0800

    refactor(shared-chat): remove header

--- src/page/shared-chat/index.tsx
+++ src/page/shared-chat/index.tsx
@@ -2,12 +2,9 @@ import { Outlet } from 'react-router-dom';
 
 import { SidebarInset } from '@/components/ui/sidebar';
 
-import Header from './header';
-
 export default function SharedChat() {
   return (
     <SidebarInset>
-      <Header />
       <Outlet />
     </SidebarInset>
   );
