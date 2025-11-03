69784ca651004244659c07885051fa7f8787fb22
hewenguang <wenguang.fe@gmail.com>
2025-11-03 17:22:14 +0800

    fix(chat): fix chat input error

--- src/page/chat/chat-input/input.tsx
+++ src/page/chat/chat-input/input.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-
-import { Textarea } from '@/components/ui/textarea';
 import type { ChatActionType } from '@/page/chat/chat-input/types';
+
+import { ChatTextarea } from './textarea';
 
 interface IProps {
   value: string;
@@ -11,45 +9,11 @@ interface IProps {
 }
 
 export default function ChatInput(props: IProps) {
-  const { t } = useTranslation();
   const { value, onChange, onAction } = props;
-  const [isComposing, setIsComposing] = React.useState(false);
-
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    onChange(e.target.value);
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === 'Enter' && !isComposing) {
-      e.preventDefault();
-      if (e.shiftKey || e.metaKey || e.ctrlKey) {
-        // @ts-ignore
-        e.target.value = e.target.value + '\n';
-      } else {
-        onAction();
-      }
-    }
-  };
-
-  const handleCompositionStart = () => {
-    setIsComposing(true);
-  };
-
-  const handleCompositionEnd = () => {
-    setIsComposing(false);
-  };
 
   return (
     <div className="mb-[2px]">
-      <Textarea
-        value={value}
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-        onCompositionStart={handleCompositionStart}
-        onCompositionEnd={handleCompositionEnd}
-        placeholder={t('chat.textarea.placeholder')}
-        className="resize-none p-0 border-transparent shadow-none focus-visible:border-transparent focus-visible:ring-0 focus-visible:shadow-none hover:border-transparent hover:shadow-none placeholder:text-[#9CA3AF] dark:placeholder:text-gray-400"
-      />
+      <ChatTextarea value={value} onChange={onChange} onAction={onAction} />
     </div>
   );
 }
