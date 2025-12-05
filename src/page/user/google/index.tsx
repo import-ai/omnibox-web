5a221cedab7228f6231e732acb5d30dd602ccf30
hewenguang <wenguang.fe@gmail.com>
2025-12-05 10:12:25 +0800

    fix(google): change popup to redirect

--- src/page/user/google/index.tsx
+++ src/page/user/google/index.tsx
@@ -1,63 +1,24 @@
-import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
 
 import { Button } from '@/components/button';
 import { http } from '@/lib/request';
-import { setGlobalCredential } from '@/page/user/util';
 
 import { GoogleIcon } from './icon';
 
 export default function Google() {
-  const { t, i18n } = useTranslation();
-  const navigate = useNavigate();
+  const { t } = useTranslation();
+
   const loginWithGoogle = () => {
     http
       .get('/google/auth-url')
       .then(authUrl => {
-        const width = 500;
-        const height = 600;
-        const left = (screen.width - width) / 2;
-        const top = (screen.height - height) / 2;
-        window.open(
-          authUrl,
-          'googleLogin',
-          `width=${width},height=${height},left=${left},top=${top},resizable=yes,scrollbars=yes`
-        );
+        window.location.href = authUrl;
       })
       .catch(error => {
         toast.error(error.message, { position: 'bottom-right' });
       });
   };
-
-  useEffect(() => {
-    const messageFN = (event: MessageEvent) => {
-      if (event.origin !== window.location.origin) {
-        return;
-      }
-      const { code, state } = event.data;
-      if (code && state) {
-        http
-          .post(`/google/callback`, {
-            code,
-            state,
-            lang: i18n.language,
-          })
-          .then(res => {
-            setGlobalCredential(res.id, res.access_token);
-            navigate('/', { replace: true });
-          })
-          .catch(error => {
-            toast.error(error.message, { position: 'bottom-right' });
-          });
-      }
-    };
-    window.addEventListener('message', messageFN);
-    return () => {
-      window.removeEventListener('message', messageFN);
-    };
-  }, []);
 
   return (
     <Button
