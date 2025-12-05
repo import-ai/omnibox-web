3498e245052441714c9e6f752c93e077e0e0a508
hewenguang <wenguang.fe@gmail.com>
2025-12-05 21:03:25 +0800

    fix(comment): fix comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-export BACKEND_OSS=${BACKEND_OSS:-http://backend:8000}
-export BACKEND_PRO=${BACKEND_PRO:-http://backend-pro:8001}
+export BACKEND=${BACKEND:-http://backend:8000}
+export BACKEND_PRO=${BACKEND_PRO:-http://backend-pro:8000}
 
 echo "Configuring nginx with:"
-echo "  BACKEND_OSS=$BACKEND_OSS (open source module)"
+echo "  BACKEND=$BACKEND (open source module)"
 echo "  BACKEND_PRO=$BACKEND_PRO (commercial module)"
 
-envsubst '${BACKEND_OSS} ${BACKEND_PRO}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND} ${BACKEND_PRO}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 echo "Starting nginx..."
 exec nginx -g "daemon off;"
