3457c483adaa74e4a764b12cb95e041b1d4f230e
hewenguang <wenguang.fe@gmail.com>
2025-12-25 10:25:08 +0800

    fix(comment): fix comments

--- src/lib/email-validation.ts
+++ src/lib/email-validation.ts
@@ -5,6 +5,5 @@ import { ALLOWED_EMAIL_DOMAINS } from '@/const';
  */
 export function isAllowedEmailDomain(email: string): boolean {
   const domain = email.split('@')[1];
-  console.log(ALLOWED_EMAIL_DOMAINS.includes(domain), domain);
   return ALLOWED_EMAIL_DOMAINS.includes(domain);
 }
