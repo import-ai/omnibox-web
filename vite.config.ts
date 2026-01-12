483d71dd160fa73687fcc8271734a274ebb9b72d
bubble06 <97491997+bubble06@users.noreply.github.com>
2026-01-12 14:50:25 +0800

    feat: Added PWA feature

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { execSync } from 'child_process';
 import { defineConfig, loadEnv } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 let gitHash = 'unknown';
 try {
@@ -18,7 +19,32 @@ export default defineConfig(({ mode }) => {
     define: {
       'import.meta.env.VITE_APP_VERSION': JSON.stringify(gitHash),
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        injectRegister: false,
+        manifest: {
+          name: 'OmniBox',
+          short_name: 'OmniBox',
+          theme_color: '#3b82f6',
+          background_color: '#ffffff',
+          display: 'standalone',
+          start_url: '/',
+          icons: [
+            {
+              src: '/icons/pwa_192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/icons/pwa_512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
+      }),
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
