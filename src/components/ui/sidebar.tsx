2fa3d4dd0e1c2da1d0de79595cf1424d86acf513
bubble06 <97491997+bubble06@users.noreply.github.com>
2026-01-13 14:05:18 +0800

    fix: Fix PWA adaptive page style

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -229,7 +229,7 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
-            <div className="flex h-full w-full flex-col touch-none-select">
+            <div className="flex h-full w-full flex-col touch-none-select pt-[env(safe-area-inset-top)]">
               {children}
             </div>
           </SheetContent>
