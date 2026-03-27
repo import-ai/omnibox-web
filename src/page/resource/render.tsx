658b8d4eb8b51b74c2bf671a1f004935591ed555
bubble06 <97491997+bubble06@users.noreply.github.com>
2026-03-27 18:32:55 +0800

    fix: Fix the table issue on the page

--- src/page/resource/render.tsx
+++ src/page/resource/render.tsx
@@ -93,7 +93,10 @@ export default function Render(props: IProps) {
   }, [search]);
 
   return (
-    <div ref={containerRef} className="pb-[30vh]">
+    <div
+      ref={containerRef}
+      className="pb-[30vh] min-w-0 max-w-full overflow-x-auto"
+    >
       <Markdown
         style={style}
         content={embedImage(resource)}
