99acaf5639c98eaa6ba8479f6c62be7cbba94725
hewenguang <wenguang.fe@gmail.com>
2026-06-04 15:01:58 +0800

    style(batch): modify toolbar style

--- src/page/sidebar/components/toolbar/Tooltip.tsx
+++ src/page/sidebar/components/toolbar/Tooltip.tsx
@@ -27,10 +27,10 @@ export function ToolbarButton({
             disabled={!!disabledLabel}
             className={cn(
               'size-[18px] rounded-sm text-[#8F959E]',
-              'hover:bg-[rgba(23,23,23,0.1)] active:bg-[rgba(23,23,23,0.15)]',
-              'disabled:cursor-not-allowed disabled:text-neutral-300',
-              'dark:text-neutral-300 dark:hover:text-muted-foreground dark:hover:bg-[rgba(255,255,255,0.1)] dark:active:text-[#fafafa] dark:active:bg-[rgba(255,255,255,0.2)] dark:disabled:text-neutral-500',
-              'disabled:opacity-100 disabled:pointer-events-auto disabled:bg-transparent'
+              'hover:text-[#8F959E] hover:bg-[rgba(23,23,23,0.1)] active:text-[#8F959E] active:bg-[rgba(23,23,23,0.15)]',
+              'disabled:cursor-not-allowed disabled:text-neutral-300 disabled:bg-transparent',
+              'dark:text-neutral-300 dark:hover:text-neutral-400 dark:hover:bg-[rgba(255,255,255,0.1)] dark:active:text-[#fafafa] dark:active:bg-[rgba(255,255,255,0.2)] dark:disabled:text-primary-foreground',
+              'disabled:opacity-100 disabled:pointer-events-auto dark:disabled:bg-transparent dark:disabled:text-neutral-700'
             )}
           >
             <Icon className="size-4" />
