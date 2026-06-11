0d93a942b08055dcea312ad8a4a40b977f8c1a1f
NaoNao <212327806+omnibox-bot@users.noreply.github.com>
2026-06-11 15:57:42 +0800

    fix(sidebar): Use locate fixed toolbar icon

--- src/page/sidebar/components/toolbar/LocateResourceIcon.tsx
+++ src/page/sidebar/components/toolbar/LocateResourceIcon.tsx
@@ -6,52 +6,68 @@ export function LocateResourceIcon({ className }: LocateResourceIconProps) {
   return (
     <svg
       className={className}
-      width="18"
-      height="18"
-      viewBox="0 0 18 18"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
     >
-      <path
-        d="M9 4.5V1.5"
+      <line
+        x1="2"
+        x2="5"
+        y1="12"
+        y2="12"
         stroke="currentColor"
-        strokeWidth="1.5"
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-      <path
-        d="M9 16.5V13.5"
+      <line
+        x1="19"
+        x2="22"
+        y1="12"
+        y2="12"
         stroke="currentColor"
-        strokeWidth="1.5"
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-      <path
-        d="M13.5 9H16.5"
+      <line
+        x1="12"
+        x2="12"
+        y1="2"
+        y2="5"
         stroke="currentColor"
-        strokeWidth="1.5"
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-      <path
-        d="M1.5 9H4.5"
+      <line
+        x1="12"
+        x2="12"
+        y1="19"
+        y2="22"
         stroke="currentColor"
-        strokeWidth="1.5"
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-      <path
-        d="M9 13.5C11.4853 13.5 13.5 11.4853 13.5 9C13.5 6.51472 11.4853 4.5 9 4.5C6.51472 4.5 4.5 6.51472 4.5 9C4.5 11.4853 6.51472 13.5 9 13.5Z"
+      <circle
+        cx="12"
+        cy="12"
+        r="7"
         stroke="currentColor"
-        strokeWidth="1.5"
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-      <path
-        d="M9 11.25C10.2426 11.25 11.25 10.2426 11.25 9C11.25 7.75736 10.2426 6.75 9 6.75C7.75736 6.75 6.75 7.75736 6.75 9C6.75 10.2426 7.75736 11.25 9 11.25Z"
+      <circle
+        cx="12"
+        cy="12"
+        r="3"
         stroke="currentColor"
-        strokeWidth="1.5"
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
