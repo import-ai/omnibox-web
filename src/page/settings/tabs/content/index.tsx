7a240ac202d0a4f90ea9f0cb477e0a1954920a22
bubble06 <97491997+bubble06@users.noreply.github.com>
2026-07-24 15:37:47 +0800

    fix(setting): Fix some titles in the settings pop-up window

--- src/page/settings/tabs/content/index.tsx
+++ src/page/settings/tabs/content/index.tsx
@@ -6,11 +6,11 @@ export default function Content() {
   const { t } = useTranslation();
 
   return (
-    <div className="flex flex-col pt-4">
+    <div className="flex flex-col">
       <h3 className="text-base font-semibold text-foreground">
         {t('setting.feature_management')}
       </h3>
-      <div className="mt-6 flex flex-col gap-2">
+      <div className="mt-3 flex flex-col gap-2">
         <AutoTag />
       </div>
     </div>
