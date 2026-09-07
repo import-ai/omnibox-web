b8e049f226b3242cffcfaee036508b72f11ef71d
zm <2215057779@qq.com>
2026-09-07 17:21:48 +0800

    fix(permissions): Prevent owner self-demotion

--- src/components/permission-action/index.tsx
+++ src/components/permission-action/index.tsx
@@ -19,7 +19,7 @@ import { Permission } from '@/interface';
 import { http } from '@/lib/request';
 
 import Action, { ActionProps } from './Action';
-import { getData } from './data';
+import { getData, getDisabledPermissions } from './data';
 
 interface IProps extends Omit<ActionProps, 'afterAddon' | 'data' | 'onChange'> {
   user_id: string;
@@ -29,6 +29,7 @@ interface IProps extends Omit<ActionProps, 'afterAddon' | 'data' | 'onChange'> {
   refetch: () => void;
   canNoAccess?: boolean;
   alertWhenDelete?: boolean;
+  isOwner?: boolean;
 }
 
 export default function PermissionAction(props: IProps) {
@@ -43,11 +44,15 @@ export default function PermissionAction(props: IProps) {
     alertWhenDelete,
     canNoAccess = true,
     canRemove = true,
+    isOwner = false,
   } = props;
   const data = getData(!canNoAccess);
   const { t } = useTranslation();
   const uid = localStorage.getItem('uid');
   const me = uid === user_id;
+  const ownerSelf = me && isOwner;
+  const currentPermissionIndex = data.findIndex(item => item.value === value);
+  const disabledPermissions = getDisabledPermissions(data, value, ownerSelf);
   const [grant, onGrant] = useState(false);
   const [remove, onRemove] = useState(false);
   const [ownerOnly, setOwnerOnly] = useState(false);
@@ -78,8 +83,14 @@ export default function PermissionAction(props: IProps) {
     return http.delete(basePath).then(refetch);
   };
   const handleChange = (permission: Permission) => {
+    if (ownerSelf) {
+      const newIndex = data.findIndex(item => item.value === permission);
+      if (newIndex > currentPermissionIndex) {
+        return;
+      }
+    }
     if (me) {
-      const oldIndex = data.findIndex(item => item.value === value);
+      const oldIndex = currentPermissionIndex;
       const newIndex = data.findIndex(item => item.value === permission);
       if (oldIndex < newIndex) {
         onPermission(permission);
@@ -152,6 +163,7 @@ export default function PermissionAction(props: IProps) {
         data={data}
         value={value}
         disabled={disabled}
+        disabledValues={disabledPermissions}
         className={className}
         onChange={handleChange}
         afterAddon={
